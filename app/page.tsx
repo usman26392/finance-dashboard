8ae385ce6e2b77432d3675ff05ba5d3405e1a92a
Muhammad Usman <usmannawaz53@gmail.com>
2025-05-01 14:02:08 +0500

    fix: remove unnecessary text from welcome message in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,7 @@ export default function Page() {
         {/* <div className={style.shape}></div> */}
 
           <p className={`${lusitana.className}  text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme.</strong> 
           </p>
           <Link
             href="/login"
